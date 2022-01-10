876a088ab65952ea121122ce8caea7c0365770f2
DrManoj001 <91949949+DrManoj001@users.noreply.github.com>
2022-01-10 11:58:32 +0530

    Add files via upload

---
+++ positiveneagtiveandzero.py
@@ -0,0 +1,12 @@
+'''
+Program to find out the number is poaitive,negative or zero
+
+
+'''
+n=int(input("Enter a number: "))
+if n==0:
+      print("The number is Zero")
+elif n>0:
+      print("The number is Positive")
+else:
+      print("The number is Negative")
